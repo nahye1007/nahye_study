4c306efa41f2d4a8f5a6cd5fe98add17db4fdec4
iserendipity <96815736+iserendipity@users.noreply.github.com>
2022-01-04 11:30:35 +0900

    Create Human.java
    name 문자열 변수, run 함수를 가진 Human 클래스 생성(과제1)

---
+++ Human.java
@@ -0,0 +1,29 @@
+/*
+   name 문자열 변수
+   run 함수
+   Human 클래스 생성
+*/
+
+public class Human{
+
+    public String name; //변수 = 상태값, String 타입을 가진 name이라는 이름의 변수
+    
+    //함수
+    public void run(){
+    
+    }
+    
+    //생성자
+    public Human(){
+    
+    }
+   
+
+
+
+
+
+
+
+
+}
